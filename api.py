2ea1933a4fcaa9a1d5af10759657e66c03807a96
arsummers <aliyasummers1@gmail.com>
2019-10-17 15:58:30 -0700

    swagger

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-# from flask_restful import Resource, Api
+import connexion
 
-app = Flask(__name__, template_folder='templates')
-# api = Api(app)
+app = connexion.App(__name__, specification_dir='./')
+
+app.add_api('swagger.yml')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
