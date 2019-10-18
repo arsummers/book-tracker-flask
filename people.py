d0c8cecdb7c4cdf69d0fb0695de118669414eb69
arsummers <aliyasummers1@gmail.com>
2019-10-18 16:16:38 -0700

    people update

--- people.py
+++ people.py
@@ -1,100 +1,87 @@
-from datetime import datetime
 from flask import make_response, abort
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp(),
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp(),
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp(),
-    },
-}
+from config import db
+from models import Person, PersonSchema
 
 def read_all():
     """
     creates list of all people
     """
-    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    people = Person.query.order_by(Person.lname).all()
 
-def read_one(lname):
+    # serialize data for response
+    person_schema = PersonSchema(many=True)
+    return person_schema.dump(people).data
+        
+
+def read_one(person_id):
     """
     searches through list of people based on name
     """
-    if lname in PEOPLE:
-        person = PEOPLE.get(lname)
+    person = Person.query.filter(Person.person_id == person_id).one_or_none()
+
+    # if person exists, serialize
+    if person is not None:
+        person_schema = PersonSchema()
+        return person_schema.dump(person).data
 
     else:
-        abort(
-            404, "Last name {lname} not found".format(lname=lname)
-        )
-    
-    return person
+        abort(404, 'Person with id {person_id} not found'.format(person_id=person_id))
 
 def create(person):
     """
     creates a new person object in the PEOPLE structure based on data that is passed in
     """
 
-    lname = person.get('lname', None)
-    fname = person.get('fname', None)
+    lname = person.get('lname')
+    fname = person.get('fname')
 
     # checks if person already exists
-    if lname not in PEOPLE and lname is not None:
-        PEOPLE[lname] = {
-            'lname':lname,
-            'fname':fname,
-            'timestamp':get_timestamp()
-        }
-        return make_response(
-            '{lname} successfully created.'.format(lname=lname), 201
-        )
+    existing_person = Person.query.filter(Person.fname == fname).filter(Person.lname == lname).one_or_none()
+
+    if existing_person is None:
+        
+        schema = PersonSchema
+        new_person = schema.load(person, session=db.session).data
+
+        db.session.add(new_person)
+        db.session.commit()
+
+        return schema.dump(new_person).data, 201
 
     else:
         abort(
-            406,
+            409,
             'Person with last name {lname} already exists'.format(lname=lname)
         )
 
-def update(lname, person):
-    """
-    updates and existing person
-    """
-    if lname in PEOPLE:
-        PEOPLE[lname]['fname'] = person.get('fname')
-        PEOPLE[lname]['timestamp'] = get_timestamp()
+# def update(lname, person):
+#     """
+#     updates and existing person
+#     """
+#     if lname in PEOPLE:
+#         PEOPLE[lname]['fname'] = person.get('fname')
+#         PEOPLE[lname]['timestamp'] = get_timestamp()
 
-        return PEOPLE[lname]
+#         return PEOPLE[lname]
 
-    else:
-        abort(
-            404,
-            "Person with name {lname} not found".format(lname=lname)
-        )
+#     else:
+#         abort(
+#             404,
+#             "Person with name {lname} not found".format(lname=lname)
+#         )
 
-def delete(lname):
-    """
-    yeets a person from the structure
-    """
-    if lname in PEOPLE:
-        del PEOPLE[lname]
-        return make_response(
-            "{lname} successsfully deleted".format(lname=lname), 200
-        )
+# def delete(lname):
+#     """
+#     yeets a person from the structure
+#     """
+#     if lname in PEOPLE:
+#         del PEOPLE[lname]
+#         return make_response(
+#             "{lname} successsfully deleted".format(lname=lname), 200
+#         )
 
-    else:
-        abort(
-            404,
-            "Person with name {lname} not found".format(lname=lname)
-        )
+#     else:
+#         abort(
+#             404,
+#             "Person with name {lname} not found".format(lname=lname)
+#         )
