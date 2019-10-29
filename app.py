fa4900686be524013d5cd2a5fa83b8525ff05d5c
arsummers <aliyasummers1@gmail.com>
2019-10-29 16:34:17 -0700

    routes

--- app.py
+++ app.py
@@ -26,9 +26,29 @@ def new_book():
         session.commit()
         return redirect(url_for('show_books'))
     else:
-        return render_template('newBook.html')
+        return render_template('new_book.html')
 
 @app.route('/books/<int:book_id>/edit/', methods=['GET', 'POST'])
 def edit_book(book_id):
     edited_book = session.query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            edit_book.title = request.form['name']
+            return redirect(url_for('show_books'))
+    else:
+        return render_template('edited_book.html', book=edited_book)
+
+@app.route('/books/<int:book_id>/delete/', methods=['GET', 'POST'])
+def delete_book(book_id):
+    book_to_delete = session.query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        session.delete(book_to_delete)
+        session.commit()
+        return redirect(url_for('show_books', book_id=book_id))
+    else:
+        return render_template('delete_book.html', book=book_to_delete)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=4996)
     
