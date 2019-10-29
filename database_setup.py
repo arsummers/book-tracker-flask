10d2653febd9456188785bd82b216e1020ae4406
arsummers <aliyasummers1@gmail.com>
2019-10-29 15:24:55 -0700

    moved realpython stuff

---
+++ database_setup.py
@@ -0,0 +1,13 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# classes will go here
+
+engine = create_engine('sqlite:///books-collection.db')
+
+Base.metadata.create_all(engine)
